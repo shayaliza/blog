59798de4d17732a7cd4df23dbd0e014228e2f7de
shayaliza <magicalmind01@gmail.comgit statuss>
2024-03-24 23:04:33 +0530

    ok

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
 
   return (
     <div className="flex justify-center items-center  p-4">
-      <div className="text-lightwhite text-3xl font3 ">Shayaliza Blog's</div>
+      <div className="text-lightwhite text-3xl font3 ">Shayaliza Blogs</div>
     </div>
   );
 }
