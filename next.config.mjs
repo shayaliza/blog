a024f2bd5add2108f8da71195da694e978730984
shayaliza <magicalmind01@gmail.comgit statuss>
2024-03-31 15:39:56 +0530

    new firebase

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  extends: "next",
-  rules: {
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off",
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
