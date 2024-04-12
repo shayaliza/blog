82f21b5d12ac1803017d8d4a9c138b0f6073e154
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-04-12 09:10:32 +0530

    homesection

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Content from "./components/content";
-import Navbar from "./components/navbar";
+import Mainpage from "./components/home";
+import Navbarr from "./components/navbar";
+
 
 export default function Home() {
   return (
     <div className="bg-bluebg h-[100vh]">
-      <Navbar />
+      <Navbarr />
+<Mainpage/>
+
       <Content />
     </div>
   );
